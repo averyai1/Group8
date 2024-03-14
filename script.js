97247717da740b47bf929ef8befb5f5146bb415e
averyai1 <104400388+averyai1@users.noreply.github.com>
2024-03-14 16:20:23 -0400

    nav bar

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//Comic sans exclussively 
+//setting blur for everything except nav bar
+const navbar = document.querySelector('.navbar');
+const content = document.querySelectorAll('body > :not(.navbar)');
+
+//toggling the blur
+function toggleBlur(on) {
+    document.body.classList.toggle('blurred', on);
+}
+
+//checks if mouse is on or off 
+navbar.addEventListener('mouseenter', () => toggleBlur(true));
+navbar.addEventListener('mouseleave', () => toggleBlur(false));
